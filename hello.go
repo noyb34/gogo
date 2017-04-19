847fefaf61452ca2142074f46227ad6c0a435997
Patrick Roch <patrick.roch@rochsystems.com>
2017-04-18 20:18:48 -0400

    initial commit

---
+++ hello.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+import "os"
+
+func main() {
+
+	if len(os.Args) > 1 {
+
+		fmt.Printf("Hello, world.\n")
+
+	} else {
+
+		fmt.Printf("Hello, P. Roch!\nThis is your first go program.\n")
+
+	}
+
+}
